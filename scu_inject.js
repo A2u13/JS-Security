9eea9183ae23763b35d4ffd0251f7d2aaea91a7b
A2u13 <38445818+A2u13@users.noreply.github.com>
2021-02-10 18:57:20 +0800

    Create scu_inject.js

--- scu_inject.js
+++ scu_inject.js
@@ -5,7 +5,7 @@ const scriptName = 'SCU_Zhjw_Helper';
 let body = null;
 let magicJS = MagicJS(scriptName, "INFO");
 
-let html = magicJS.response.body;
+let html = magicJS.response;
 magicJS.logInfo(`Info: ${html} `);
 
 $done({ body })
